56056a21f853d76de8e71be48e4b7d97c9a4482b
grimza99 <gbtmxlf@naver.com>
2026-07-13 12:32:35 +0900

    refactor : 기존에 AppContent로 토스트 프로바이더 범위 문제를 우회하던것 원복

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import { ToastProvider } from "./components/shell/toast/ToastProvider";
 import NotFoundRoomPage from "./pages/NotFoundRoomPage";
 import { useRouteState } from "./lib/router";
 
-function AppContent() {
+export default function App() {
   const [visibleMonth, setVisibleMonth] = useState("");
 
   const { route } = useRouteState();
@@ -20,36 +20,26 @@ function AppContent() {
   }, [route]);
 
   return (
-    <>
-      {route.name === "landing" ? (
-        <LandingPage setVisibleMonth={setVisibleMonth} />
-      ) : route.name === "report" ? (
-        <ReportPage />
-      ) : route.name === "room_access_restricted" ? (
-        <RoomAccessRestrictedPage />
-      ) : route.name === "not-found-room" ? (
-        <NotFoundRoomPage />
-      ) : (
-        <RoomPage
-          setVisibleMonth={setVisibleMonth}
-          visibleMonth={visibleMonth}
-        />
-      )}
-      {route.name !== "report" && <ReportEntryButton />}
-    </>
-  );
-}
-
-function App() {
-  return (
     <div className="shell">
       <div className="mobile-frame">
         <ToastProvider>
-          <AppContent />
+          {route.name === "landing" ? (
+            <LandingPage setVisibleMonth={setVisibleMonth} />
+          ) : route.name === "report" ? (
+            <ReportPage />
+          ) : route.name === "room_access_restricted" ? (
+            <RoomAccessRestrictedPage />
+          ) : route.name === "not-found-room" ? (
+            <NotFoundRoomPage />
+          ) : (
+            <RoomPage
+              setVisibleMonth={setVisibleMonth}
+              visibleMonth={visibleMonth}
+            />
+          )}
+          {route.name !== "report" && <ReportEntryButton />}
         </ToastProvider>
       </div>
     </div>
   );
 }
-
-export default App;
